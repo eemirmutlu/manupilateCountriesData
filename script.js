6df1e9bfbce29099ed7d4ecc0260998b0340c61a
Emir Mutlu <emirmutlu169@icloud.com>
2023-11-12 11:00:33 +0300

    html sayfama eklediğim inputu js yardımı ile placeholderını düzenledim

---
+++ script.js
@@ -0,0 +1,8 @@
+for (let i = 0; i < countries.length; i++) {
+    console.log(countries[i]["name"])
+}
+
+let input = document.getElementById("input")
+
+input.setAttribute("placeholder", "Değerim")
+// input.removeAttribute("placeholder")
