ffa69ccdbe2394d78d8afdf6d2e2a9b5628a8639
Emir Mutlu <emirmutlu169@icloud.com>
2023-11-12 11:34:36 +0300

    js üzerinden etiket oluşturma

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ let input = document.getElementById("input")
 
 input.setAttribute("placeholder", "Değerim")
 // input.removeAttribute("placeholder")
+
+const div = document.createElement("div")
+div.style.width = "300px"
+div.style.height = "300px"
+div.style.border = "1px solid black"
+div.style.borderRadius = "10px"
+
+// document.body.append(div)
+document.body.appendChild(div)
